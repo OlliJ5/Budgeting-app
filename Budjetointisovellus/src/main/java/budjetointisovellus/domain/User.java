e9789444d0d17e60d8e8934183f3c8fdb79346cd
Olli Rousu <olli.rousu@hotmail.com>
2018-04-05 13:50:23 +0300

    kirjautumisnäkymä luotu

--- Budjetointisovellus/src/main/java/budjetointisovellus/domain/User.java
+++ Budjetointisovellus/src/main/java/budjetointisovellus/domain/User.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package budjetointisovellus.domain;
 
-/**
- *
- * @author ogrousu
- */
+
 public class User {
+    private String username;
+    private String name;
+
+    public User(String username, String name) {
+        this.username = username;
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public String getUsername() {
+        return this.username;
+    }
     
 }
--- Budjetointisovellus/src/main/java/budjetointisovellus/ui/Main.java
+++ Budjetointisovellus/src/main/java/budjetointisovellus/ui/Main.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package budjetointisovellus.ui;
 
-/**
- *
- * @author ogrousu
- */
-public class Main {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Budjetointisovellus");
+        Label nameText = new Label("Nimi: ");
+        Label usernameText = new Label("Käyttäjätunnus: ");
+        TextField nameField = new TextField();
+        TextField usernameField = new TextField();
+        
+        Button addButton = new Button("Luo käyttäjä");
+        
+        GridPane pane = new GridPane();
+        pane.add(nameText, 0, 0);
+        pane.add(nameField, 1, 0);
+        pane.add(usernameText, 0, 1);
+        pane.add(usernameField, 1, 1);
+        pane.add(addButton, 1, 2);
+        
+        pane.setHgap(10);
+        pane.setVgap(10);
+        pane.setPadding(new Insets(50, 50, 50, 10));
+        
+        Scene createUser = new Scene(pane);
+        primaryStage.setScene(createUser);
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
     
 }
