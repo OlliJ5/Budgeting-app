723596826cf35c9da0feeae1a935b9fafed1a022
Olli Rousu <olli.rousu@hotmail.com>
2018-04-08 18:56:13 +0300

    testejä aloiteltu

---
+++ Budjetointisovellus/src/test/java/domainTest/UserTest.java
@@ -0,0 +1,32 @@
+package domainTest;
+
+import budjetointisovellus.domain.User;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class UserTest {
+
+    User user;
+
+    @Before
+    public void setUp() {
+        user = new User("ogrousu", "Olli", 100.0);
+    }
+    
+    @Test
+    public void constructorSetsCorrectUsername() {
+        assertEquals("ogrousu", user.getUsername());
+    }
+
+    @Test
+    public void constructorSetsCorrectName() {
+        assertEquals("Olli", user.getName());
+    }
+    
+    @Test
+    public void constructorSetsCorretBudget() {
+        assertEquals(100.0, user.getBudget(), 0.01);
+    }
+}
